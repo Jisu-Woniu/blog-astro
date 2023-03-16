52e578b10f5b70b468a59c12aa53dffcb8fd9e6e
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2023-03-16 17:15:35 +0800

    Add 404 and PurgeCSS.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import remarkSmartypants from "remark-smartypants";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import prismjsPlugin from "vite-plugin-prismjs";
-
+import PurgeCSS from "vite-plugin-purgecss";
 import lit from "@astrojs/lit";
 
 // https://astro.build/config
@@ -24,6 +24,12 @@ export default defineConfig({
         languages: ["html"],
         // "all",
         plugins: ["line-numbers", "toolbar", "copy-to-clipboard"],
+      }),
+      PurgeCSS({
+        safelist: {
+          standard: [/code/, /toolbar/, /line-numbers/],
+          deep: [/button/],
+        },
       }),
     ],
   },
--- src/content/blog/My-own-post.md
+++
@@ -1,9 +0,0 @@
----
-title: "My Next Post"
-pubDate: 2023-03-13
-description: "Newly created blog"
-tags:
-  - "astro"
-  - "blogging"
-  - "learning in public"
----
