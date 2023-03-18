80779964be69e1dac103982850964adb2c7ad67c
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2023-03-18 11:36:56 +0800

    Use sorted content collection.

---
+++ src/utils/ContentUtils.ts
@@ -0,0 +1,8 @@
+import { getCollection } from "astro:content";
+const getSortedBlogCollection = () =>
+  getCollection("blog").then((collection) =>
+    collection.sort(
+      (a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime()
+    )
+  );
+export { getSortedBlogCollection };
