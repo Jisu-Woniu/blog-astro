6ed37b4dcf75826e8121cb0bad16336ea910acb3
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2025-03-12 01:15:09 +0800

    style: 🎨 replace Prettier with Biome

--- src/utils/ContentUtils.ts
+++ src/utils/ContentUtils.ts
@@ -2,7 +2,7 @@ import { getCollection } from "astro:content";
 const getSortedBlogCollection = () =>
   getCollection("blog").then((collection) =>
     collection.sort(
-      (a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime()
-    )
+      (a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime(),
+    ),
   );
 export { getSortedBlogCollection };
